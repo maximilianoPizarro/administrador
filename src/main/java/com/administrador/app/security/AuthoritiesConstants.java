84cf14ccf6db242ea7d8a12203de383ca7794885
mpizarro <maximilianopi@buenosaires.gob.ar>
2020-10-12 13:56:03 -0300

    agrega roles estudiante y profesor

--- src/main/java/com/administrador/app/security/AuthoritiesConstants.java
+++ src/main/java/com/administrador/app/security/AuthoritiesConstants.java
@@ -9,6 +9,10 @@ public final class AuthoritiesConstants {
 
     public static final String USER = "ROLE_USER";
 
+    public static final String ESTUDIANTE = "ROLE_ESTUDIANTE";
+
+    public static final String PROFESOR = "ROLE_PROFESOR";
+
     public static final String ANONYMOUS = "ROLE_ANONYMOUS";
 
     private AuthoritiesConstants() {
